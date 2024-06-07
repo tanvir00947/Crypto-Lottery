a54f604cc2840dac68cbfc842cb7931a3249d7c8
tanvir00947 <tanvir00947@gmail.com>
2024-06-08 01:04:47 +0530

    now without metamast users can see lottery

--- src/Home.js
+++ src/Home.js
@@ -7,17 +7,27 @@ function Home() {
     const [currentAccount, setCurrentAccount] = useState("");
     const [contractInstance, setContractInstance] = useState(null);
     const [status, setStatus] = useState(false);
-    const [winner,setWinner] = useState("");
+    const [winner, setWinner] = useState("");
     const [isWinner, setIsWinner] = useState(false);
     const [isOwner, setIsOwner] = useState(false);
     const [claimed, setClaimed] = useState(false);
     const [players, setPlayers] = useState([]);
     const [manager, setManager] = useState("");
+    const [metaMaskInstalled, setMetaMaskInstalled] = useState(false);
+
+    const [loadingManager, setLoadingManager] = useState(true);
+    const [loadingPlayers, setLoadingPlayers] = useState(true);
+    const [loadingStatus, setLoadingStatus] = useState(true);
+    const [loadingWinner, setLoadingWinner] = useState(true);
+    const [loadingClaimed, setLoadingClaimed] = useState(true);
 
     useEffect(() => {
         const loadBlockchainData = async () => {
+            let provider;
             if (typeof window.ethereum !== 'undefined') {
-                const provider = new ethers.providers.Web3Provider(window.ethereum);
+                // MetaMask is available
+                provider = new ethers.providers.Web3Provider(window.ethereum);
+                setMetaMaskInstalled(true);
                 try {
                     await provider.send("eth_requestAccounts", []);
                     const signer = provider.getSigner();
@@ -26,38 +36,44 @@ function Home() {
                     window.ethereum.on('accountsChanged', (accounts) => {
                         setCurrentAccount(accounts[0]);
                     });
-
-                    const contractIns = new ethers.Contract(constants.contractAddress, constants.contractAbi, signer);
-                    setContractInstance(contractIns);
-
-                    const status = await contractIns.isComplete();
-                    setStatus(status);
-                    const winner = await contractIns.getWinner();
-                    setWinner(winner);
-                    setIsWinner(winner === address);
-                    const owner = await contractIns.getManager();
-                    setIsOwner(owner === address);
-                    setManager(owner);  // Set the manager's address
-                    const prizeClaimed = await contractIns.claimed();
-                    setClaimed(prizeClaimed);
-                    const playersList = await contractIns.getPlayers();
-                    setPlayers(playersList);
-
-                    // console.log("Status:", status);
-                    // console.log("Winner:", winner);
-                    // console.log("Owner:", owner);
-                    // console.log("Claimed:", prizeClaimed);
-                    // console.log("Players:", playersList);
+                    provider = signer; // use the signer for actions requiring user interaction
                 } catch (err) {
                     console.error(err);
                 }
             } else {
-                alert('Please install MetaMask to use this application');
+                // MetaMask is not available, use Infura
+                provider = new ethers.providers.JsonRpcProvider('https://sepolia.infura.io/v3/afae28b7906e4634b267f00ca8c9e75e');
+                setMetaMaskInstalled(false);
+            }
+
+            const contractIns = new ethers.Contract(constants.contractAddress, constants.contractAbi, provider);
+            setContractInstance(contractIns);
+
+            try {
+                const status = await contractIns.isComplete();
+                setStatus(status);
+                setLoadingStatus(false);
+                const winner = await contractIns.getWinner();
+                setWinner(winner);
+                setIsWinner(winner === currentAccount);
+                setLoadingWinner(false);
+                const owner = await contractIns.getManager();
+                setIsOwner(owner === currentAccount);
+                setManager(owner);  // Set the manager's address
+                setLoadingManager(false);
+                const prizeClaimed = await contractIns.claimed();
+                setClaimed(prizeClaimed);
+                setLoadingClaimed(false);
+                const playersList = await contractIns.getPlayers();
+                setPlayers(playersList);
+                setLoadingPlayers(false);
+            } catch (err) {
+                console.error(err);
             }
         };
 
         loadBlockchainData();
-    }, []);
+    }, [currentAccount]);
 
     const enterLottery = async () => {
         const amountToSend = ethers.utils.parseEther('0.001');
@@ -82,70 +98,79 @@ function Home() {
     return (
         <div className="container">
             <div className="lottery-details">
-            <h1 style={{ 
-            fontFamily: 'Monotype Corsiva',
-            fontSize: '3rem', // Adjust font size as needed
-            color: '#556b2f', // Coral color
-            textAlign: 'center', // Center align text
-            textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', // Add a subtle text shadow
-            margin: '20px 0', // Add margin for spacing
-            }}>
-            Crypto-Lottery
-            </h1>
-
-
-            <table >
-                <thead>
-                    <tr>
-                    <th colSpan={2} style={{ textAlign: 'center' }}>Lottery Details</th>
-
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td>Manager:</td>
-                    <td>{manager.substring(0,5)}...{manager.substring(manager.length-6,manager.length-1)}</td>
-                    </tr>
-                    <tr>
-                    <td>Total Amount:</td>
-                    <td>{players.length * 0.001} ETH</td>
-                    </tr>
-                    <tr>
-                    <td>Entry Amount:</td>
-                    <td>0.001 ETH</td>
-                    </tr>
-                    <tr>
-                    <td>Players Count:</td>
-                    <td>{players.length}</td>
-                    </tr>
-                </tbody>
+                <h1 style={{ 
+                    fontFamily: 'Monotype Corsiva',
+                    fontSize: '3rem', 
+                    color: '#2c4820', 
+                    textAlign: 'center',
+                    textShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', 
+                    margin: '20px 0',
+                }}>
+                    Crypto-Lottery
+                </h1>
+                <table>
+                    <thead>
+                        <tr>
+                            <th colSpan={2} style={{ textAlign: 'center' }}>Lottery Details</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Manager:</td>
+                            <td>{loadingManager ? "Loading..." : `${manager.substring(0, 5)}...${manager.substring(manager.length - 6, manager.length - 1)}`}</td>
+                        </tr>
+                        <tr>
+                            <td>Total Amount:</td>
+                            <td>{loadingPlayers ? "Loading..." : `${players.length * 0.001} ETH`}</td>
+                        </tr>
+                        <tr>
+                            <td>Entry Amount:</td>
+                            <td>0.001 ETH</td>
+                        </tr>
+                        <tr>
+                            <td>Players Count:</td>
+                            <td>{loadingPlayers ? "Loading..." : players.length}</td>
+                        </tr>
+                    </tbody>
                 </table>
 
                 <div className="button-container">
-                    {status ? (
+                    {loadingStatus ? (
+                        <p>Loading status...</p>
+                    ) : status ? (
                         isWinner ? (
                             <div>
-                                {claimed ? (<p>Lottery has been sent to you, wait for the manager to Restart the Lottery.</p>):(
-                                    <div>Congratualations!!! you are the winner <button className="claim-button" onClick={claimPrize}> Claim Prize </button></div>
+                                {loadingClaimed ? (
+                                    <p>Loading claim status...</p>
+                                ) : claimed ? (
+                                    <p>Lottery has been sent to you, wait for the manager to Restart the Lottery.</p>
+                                ) : (
+                                    <div>Congratulations!!! You are the winner <button className="claim-button" onClick={claimPrize}> Claim Prize </button></div>
                                 )}
-                                
                             </div>
                         ) : (
                             <div>
-                            <p>{winner.substring(0,5)}...{winner.substring(winner.length-6,winner.length-1)} is the Winner of this lottery.</p>
-                            <p>Try again in the next Lottery. Good Luck</p>
+                                {loadingWinner ? (
+                                    <p>Loading winner...</p>
+                                ) : (
+                                    <>
+                                        <p>{winner.substring(0, 5)}...{winner.substring(winner.length - 6, winner.length - 1)} is the Winner of this lottery.</p>
+                                        <p>Try again in the next Lottery. Good Luck</p>
+                                    </>
+                                )}
                             </div>
-                            
                         )
                     ) : (
-                        <button className="enter-button" onClick={enterLottery}> Enter Lottery </button>
-                    )}
+                        <div>
+                        {metaMaskInstalled ? (
+                                <button className="enter-button" onClick={enterLottery}> Enter Lottery </button>
+                            ) : (
+                                <p>Please install and connect MetaMask to participate in this Lottery</p>
+                            )}
+                        </div>
+                        )}
+                        
                 </div>
-                {/* {isOwner && claimed && (
-                    <div className="button-container">
-                        <button className="reset-button" onClick={resetLottery}> Reset Lottery </button>
-                    </div>
-                )} */}
             </div>
             <PlayersList players={players} />
         </div>
